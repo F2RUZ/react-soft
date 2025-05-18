efb2e59a0f27ca29462ba921f56ad6aafa5d0baa
anubis <anubis@satsura.arch>
2025-05-18 11:54:53 +0500

    nimadir

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Intro from "./Intro";
+import Service from "./Service";
+import How from "./How";
+import Footer from "./Footer";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Intro />
+      <Service />
+      <How />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Intro.jsx
@@ -0,0 +1,11 @@
+const Intro = () => {
+  return (
+    <div className="intro">
+      <h1>Welcome to My React App</h1>
+      <p>This is a simple React application.</p>
+      <p>It is designed to demonstrate the basic structure of a React app.</p>
+    </div>
+  );
+};
+
+export default Intro;
